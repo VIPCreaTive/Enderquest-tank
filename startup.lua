e2e4c1de1c920fe410c37e0393b594c5317f6f67
VIPCreaTive <philipp.auler@gmx.de>
2019-01-19 21:11:57 +0100

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,35 +1,123 @@
 ChestCount = 0
-ChestNumber = {}
 Chest = {}
-ChestInfo = {}
+Chestnumber = 0
+color = ""
 
-
-
-function newchest()
-
-monitor.clear()
-monitor.setCursorPos(1,1)
-print("Geben Sie einen Chestnamen ein!")
-monitor.write("Chestname: ")
-local input = read()
-Chest[ChestCount + 1]ChestInfo[1] = input
-
-monitor.setCursorPos(1,1)
-print("Geben Sie die erste Farbe ein!")
-monitor.write("Farbe Nr.1: ")
-local input = read()
-Chest[ChestCount + 1]ChestInfo[2] = input
-
-print(Chest[1][1])
-print(Chest[1][2])
-
+for x=1,30 do
+Chest[x] = {}
 end
 
+function loadvar()
+	local file = fs.open("saves/variables","r")
+	local fileData = {}
+	local line = file.readLine()
+repeat
+	table.insert(fileData,line)
+	line = file.readLine()
+until line == nil
+file.close()
+	ChestCount = fileData[1]
+end
 
+function farben()
+print("")
+print("white	1	-	lightGray	256")
+print("orange	2	-	cyan	512")
+print("magenta	4	-	purple	1024")
+print("lightBlue	8	-	blue	2048")
+print("yellow	16	-	brown	4096")
+print("lime	32	-	green	8192")
+print("pink	64	-	red	16384")
+print("gray	128	-	black	32768")
+print("")
+end
 
+function newchest()
+monitor.clear()
+monitor.setCursorPos(1,1)
+Chestnumber = ChestCount + 1
+print("Geben Sie einen Chestnamen ein!	Neue Nummer wird : "..Chestnumber.." ")
+monitor.write("Chestname: ")
+Chestname = read()
+
+Chest[Chestnumber][1] = Chestname
+
+farben()
+print("Geben Sie die erste Farbe ein!")
+Chestcolor1 = read()
+Chest[Chestnumber][2] = Chestcolor1
+
+print("Geben Sie die zweite Farbe ein!")
+Chestcolor2 = read()
+Chest[Chestnumber][3] = Chestcolor2
+
+print("Geben Sie die dritte Farbe ein!")
+Chestcolor3 = read()
+Chest[Chestnumber][4] = Chestcolor3
+
+--Create Directory
+fs.makeDir("saves")
+--Create/Update File
+setchest()
+
+--Update Variables
+updatevar()
+
+--More Chests?
+monitor.clear()
+print("Moechten Sie weitere Chests hinzufuegen? (y/n)")
+local input = read()
+	if input == "y" then
+		newchest()
+	elseif input == "n" then
+		chest()
+	else
+		print("Ungueltige Eingabe! Zeige Chests an")
+		chest()
+	end
+end
+
+function setchest()
+local file = fs.open("saves/"..Chestnumber,"w")
+file.writeLine(Chestname)
+file.close()
+local file = fs.open("saves/"..Chestnumber,"a")
+file.writeLine(Chestcolor1)
+file.writeLine(Chestcolor2)
+file.writeLine(Chestcolor3)
+file.close()
+end
+
+function updatevar()
+local file = fs.open("saves/variables","w")
+ChestCount = ChestCount + 1
+file.writeLine(ChestCount)
+file.close()
+end
+
+function readchests()
+clear()
+for x=1,ChestCount do
+--repeat
+local file = fs.open("saves/"..(x),"r")
+local fileData = {}
+local line = file.readLine()
+repeat
+table.insert(fileData,line)
+line = file.readLine()
+until line == nil
+file.close()
+	--Übergabe Chestdaten aus Datei
+	for y=1,4 do
+		Chest[x][y] = fileData[y]
+		--prints all Chestdata
+		--print((Chest[x][y]))
+	end
+end
+chest()
+end
 
 function chest()
-monitor.clear()
 monitor.setTextColor(colors.white)
 monitor.setCursorPos(3,2)
 monitor.write("Chest")
@@ -46,25 +134,124 @@ monitor.write("|")
 end
 
 --Chest
+for x=1,ChestCount do 
+	monitor.setCursorPos(3,(x) + 3)
+	monitor.setTextColor(1)
+	monitor.write((x) .. "  " .. (Chest[x][1]))
+	
+	monitor.setCursorPos(17,(x) + 3)
+	monitor.setTextColor(tonumber(Chest[x][2]))
+	monitor.write("[]")
+	
+	monitor.setTextColor(tonumber(Chest[x][3]))
+	monitor.write("[]")
+	
+	monitor.setTextColor(tonumber(Chest[x][4]))
+	monitor.write("[]")
+end
+tank()
+end
 
+function updatechest()
+clear()
+print("Welche Chest soll geaendert werden? (1-"..ChestCount..")")
+Chestnumber = tonumber(read())
+for x=1,1 do
+--repeat
+local file = fs.open("saves/"..(Chestnumber),"r")
+local fileData = {}
+local line = file.readLine()
+repeat
+table.insert(fileData,line)
+line = file.readLine()
+until line == nil
+file.close()
+	--Übergabe Chestdaten aus Datei
+	for y=1,4 do
+		Chest[Chestnumber][y] = fileData[y]
+	end
+--end
+end
+print("Geben Sie einen Chestnamen ein!")
+print("Alter Name : "..(Chest[Chestnumber][1]).." ")
+Chestname = read()
+Chest[Chestnumber][1] = Chestname
 
+farben()
+print("Geben Sie die erste Farbe ein!")
+print("Alte Farbe: "..(Chest[Chestnumber][2]))
+Chestcolor1 = read()
+Chest[Chestnumber][2] = Chestcolor1
+
+print("Geben Sie die zweite Farbe ein!")
+print("Alte Farbe: "..(Chest[Chestnumber][3]))
+Chestcolor2 = read()
+Chest[Chestnumber][3] = Chestcolor2
+
+print("Geben Sie die dritte Farbe ein!")
+print("Alte Farbe: "..(Chest[Chestnumber][4]))
+Chestcolor3 = read()
+Chest[Chestnumber][4] = Chestcolor3
+	
+setchest()
+start()
 end
 
 function tank()
 --Tank
-monitor.setTextColor(colors.orange)
 monitor.setCursorPos(29,4)
-monitor.write("[][][]  inaktiv-Lava")
+monitor.write("Cryo")
+monitor.setCursorPos(43,4)
+monitor.setTextColor(colors.orange)
+monitor.write("[]")
+monitor.setTextColor(colors.white)
+monitor.write("[]")
+monitor.setTextColor(colors.orange)
+monitor.write("[]")
+start()
 end
 
-while true do
---Left Monitor
-monitor= peripheral.wrap("left")
-newchest()
---tank()
---sleep(3)
---Right Monitor
-monitor= peripheral.wrap("right")
-newchest()
---tank()
+function start()
+print(ChestCount)
+print("Was soll gemacht werden?")
+--print("a) - Daten anzeigen")
+print("h) - Daten hinzufuegen")
+print("l) - Daten lesen/anzeigen")
+print("u) - Update Chest")
+local input = read()
+if input == "a" then
+		chest()
+	elseif input == "h" then
+		newchest()
+	elseif input == "l" then
+		readchests()
+	elseif input == "u" then
+		updatechest()
+	else 
+		print("ungueltige Eingabe")
+	end
+end
+
+function keepalive()	
+	monitor = peripheral.wrap("right")
+	tank()
+	sleep(3)
+	chest()
+start()
+end
+
+function clear()
+monitor.clear()
+term.clear()
+term.setCursorPos(1,1)
+end
+
+for x=1,1 do
+loadvar() --load variables
+monitor = peripheral.wrap("right")
+	if (tonumber(ChestCount)) > 0 then
+		readchests()
+	else
+		start()
+	end
 end
